f28f94ffdf62891ae4bd85bc485e6b0d65615856
denyss <d.pnsw4444@gmail.com>
2025-02-18 19:25:02 +0200

    Add files via upload

---
+++ class.1.py
@@ -0,0 +1,21 @@
+class Denys:
+    def __init__(self, name=None, last_name=None, birth_year=None):
+        self.name = name
+        self.last_name = last_name
+        self.birth_year = birth_year
+
+    def calculates_the_course(self, year: int):
+        if self.birth_year == None:
+            return None
+        my_course = year - (self.birth_year + 16)
+        return my_course
+
+    def set_name(self):
+        return [self.name, self.last_name]
+
+
+name_last_name = Denys('Denys', 'Panasiyk', 2007)
+list_name = name_last_name.set_name()
+my_course = name_last_name.calculates_the_course(2025)
+print(f'По розрахункам, ви навчаєтесь приблизно на курсі {my_course}')
+print(list_name)
