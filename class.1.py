2a5136651999864b314f6f20bfdab75969054ce4
denyss <d.pnsw4444@gmail.com>
2025-06-12 00:20:16 +0300

    class.1.py

--- class.1.py
+++ class.1.py
@@ -1,4 +1,4 @@
-class Roman:
+class Denys:
     def __init__(self, name=None, last_name=None, birth_year=None):
         self.name = name
         self.last_name = last_name
@@ -14,7 +14,7 @@ class Roman:
         return [self.name, self.last_name]
 
 
-name_last_name = Roman('Roman', 'Rudnik', 2008)
+name_last_name = Roman('Denys', 'Panasiyk', 2008)
 list_name = name_last_name.set_name()
 my_course = name_last_name.calculates_the_course(2025)
 print(f'По розрахункам, ви навчаєтесь приблизно на курсі {my_course}')
